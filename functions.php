c1e21b458e7f0e27973d23f3adf4873ad12f9836
irenetjrs <irkapidgorodetska@gmail.com>
2023-11-15 11:18:28 +0200

    blog added without styles

--- functions.php
+++ functions.php
@@ -26,6 +26,8 @@ add_action('wp_enqueue_scripts', 'load_js');
 //Theme Options
 
 add_theme_support('menus');
+add_theme_support('post-thumbnails');
+add_theme_support('widgets');
 
 
 //Menus
@@ -40,3 +42,11 @@ register_nav_menus(
 
 
 );
+
+
+
+
+//Custom Image Sizes
+
+add_image_size('blog-large', 800, 400, false);
+add_image_size('blog-small', 300, 200, true);
--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -1,8 +1,6 @@
 <?php if(have_posts() ): while( have_posts() ): the_post();?>
 
+   <p><?php echo get_the_date('d/m/y');?></p>
    <?php the_content();?>
    
 <?php endwhile; else: endif;?>
-
-
-
